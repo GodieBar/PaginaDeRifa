8c84a2b0886d1c100d805278ae0cdf12596fc2b0
Godiebar <godiebarbosaparra@gmail.com>
2025-06-21 21:39:23 -0500

    rifa

--- firebase.js
+++ firebase.js
@@ -13,12 +13,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDpT-6nQxRVy8OWTQYIBTV3kqbv4YoYc7Q",
+  authDomain: "rifa-6.firebaseapp.com",
+  projectId: "rifa-6",
+  storageBucket: "rifa-6.firebasestorage.app",
+  messagingSenderId: "747276157838",
+  appId: "1:747276157838:web:dae9294b8391417f3f1adf",
+  measurementId: "G-ZWKJ5ZNG4Q",
 };
 
 const app = initializeApp(firebaseConfig);
